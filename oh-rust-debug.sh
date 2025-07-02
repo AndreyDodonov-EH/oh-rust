71ad916f76405782ed440833dc2fda0efaf9b279
Andrey Dodonov <Andrey.Dodonov@endress.com>
2025-07-02 11:58:02 +0200

    Correct debug script, it should link the toolchain

--- oh-rust-debug.sh
+++ oh-rust-debug.sh
@@ -2,5 +2,6 @@
 # _oh-rust-debug.sh
 
 # Build the Rust compiler (stage 2 means just build it with the installed compiler)
-./x.py build --stage 1
+./x.py build --stage 1 library
+rustup toolchain link stage1 build/host/stage1
 rustup default stage1
